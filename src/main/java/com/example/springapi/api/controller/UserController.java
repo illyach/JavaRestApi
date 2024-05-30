a93193cf53c590aaa819b487c42ba2dcf02f8579
illyach <90748891+illyach@users.noreply.github.com>
2024-05-30 18:44:43 +0300

    f

---
+++ src/main/java/com/example/springapi/api/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.springapi.api.controller;
+
+import com.example.springapi.api.model.User;
+import com.example.springapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:8100")  // Ваш фронтенд адрес
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id){
+        Optional<User> user = userService.getUser(id);
+        return user.orElse(null);
+    }
+}
